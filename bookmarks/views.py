84b01692f0f3b699ced19feec82968ec667973e3
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-21 14:50:43 +0200

    Add BookmarkList view to bookmarks/views.py

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from photofootapi.permissions import IsOwnerOrReadOnly
+from .models import Bookmark
+from .serializers import BookmarkSerializer
 
-# Create your views here.
+
+class BookmarkList(generics.ListCreateAPIView):
+    """Bookmark List view"""
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = BookmarkSerializer
+    queryset = Bookmark.objects.all()
+
+    def perform_create(self, serializer):
+        """Create Bookmark"""
+        serializer.save(owner=self.request.user)
