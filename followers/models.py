1a77e9942a94aeff4572237a791beb5cf91b6f3a
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-21 16:57:03 +0200

    Add Follower model to followers/models

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    """
+    Follower model - related to 'owner' and 'followed'
+    'owner' = a User that is following another User
+    'followed' = a User that is being followed by 'owner'
+    We use related_name to differentiate between fields with greater ease.
+    """
+    owner = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE
+        )
+    followed = models.ForeignKey(
+        User, related_name='followed', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Data Structuring"""
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']  # Helps stop duplication
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
