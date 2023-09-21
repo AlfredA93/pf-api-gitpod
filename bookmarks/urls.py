d26d14cbea6c901dfa8e03bdd0b5f78c6de33ba6
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-21 14:54:16 +0200

    Edit BookmarkList url. Add BookmarkDetail url.

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from bookmarks import views
 
 urlpatterns = [
-    path('likes/', views.BookmarkList.as_view()),
+    path('bookmarks/', views.BookmarkList.as_view()),
+    path('bookmarks/<int:pk>', views.BookmarkDetail.as_view()),
 ]
