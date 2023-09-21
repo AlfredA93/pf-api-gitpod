a0b8d1cc96ae60ec7cf75199e58888c33820bb10
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-21 10:06:18 +0200

    Add permissions & perform_create to PostList view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from photofootapi.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -8,5 +8,10 @@ class PostList(generics.ListCreateAPIView):
     """
     List posts or create a post if logged in
     """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        """Assigns user to owner field"""
+        serializer.save(owner=self.request.user)
