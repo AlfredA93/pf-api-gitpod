dbe00a5df527b796cb5f907081bb8f09de9886d0
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-21 08:51:09 +0200

    Add ProfileSerializer to ProfileList view

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
 from .models import Profile
+from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
     """Display a list of all profiles"""
+    serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
