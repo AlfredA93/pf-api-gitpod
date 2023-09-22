8cff525d1c9e955f4a4ea9c79a4a660c26934f12
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-22 13:29:36 +0200

    Add contact email constants - settings.py

--- photofootapi/settings.py
+++ photofootapi/settings.py
@@ -103,6 +103,12 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.live.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
