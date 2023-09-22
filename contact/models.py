5e9cb1bc3a55e39c6f8c58548098b8077e38ee8e
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-22 13:27:43 +0200

    Edit Message model. Migrate Message model to db

--- contact/models.py
+++ contact/models.py
@@ -8,6 +8,7 @@ class Message(models.Model):
     email = models.EmailField()
     subject = models.CharField(max_length=120)
     message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.name}: {self.subject}'
