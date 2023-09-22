4ff6bf0650a56031a0073af76cecc39b2587957c
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-22 11:18:57 +0200

    Add Message model to contact/models.py

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.mail import send_mail
 
-# Create your models here.
+
+class Message(models.Model):
+    """Contact Us message model"""
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+    subject = models.CharField(max_length=120)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}: {self.subject}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        # Thank you email to user
+        send_mail(
+            subject="PhotoFootprint | Thank you for contacting us",
+            message="Thank you for contacting us, we'll get back to you soon",
+            from_email="photofootprintcontact@gmail.com",
+            recipient_list=[self.email],
+        )
