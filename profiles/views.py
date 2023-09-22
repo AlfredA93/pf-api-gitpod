828c12b5b004033947655248f512a2912d60ab73
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-22 14:59:03 +0200

    Add filters and ordering fields - ProfileList view

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from rest_framework import generics
+from rest_framework import generics, filters
 from photofootapi.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -13,6 +13,18 @@ class ProfileList(generics.ListAPIView):
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
+
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at'
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
