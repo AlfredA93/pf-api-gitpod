29f687e13623e1f610537d9606ffa8ba36d70442
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-22 14:55:40 +0200

    Edit ProfileList queryset. Add posts/follow counts

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import generics
 from photofootapi.permissions import IsOwnerOrReadOnly
 from .models import Profile
@@ -7,7 +8,11 @@ from .serializers import ProfileSerializer
 class ProfileList(generics.ListAPIView):
     """Display a list of all profiles"""
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
