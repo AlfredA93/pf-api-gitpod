30135f0a38769d80f86ccf12465a48eb377c3efa
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-22 10:27:12 +0200

    Refactor url patterns - each installed app/urls.py

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from bookmarks import views
 
 urlpatterns = [
-    path('bookmarks/', views.BookmarkList.as_view()),
-    path('bookmarks/<int:pk>', views.BookmarkDetail.as_view()),
+    path('', views.BookmarkList.as_view()),
+    path('<int:pk>/', views.BookmarkDetail.as_view()),
 ]
