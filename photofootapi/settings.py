6fb30b4795847a6efb2ac470a8bfa2aee6e9d204
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-22 14:20:00 +0200

    Change SECRET_KEY value in settings.py

--- photofootapi/settings.py
+++ photofootapi/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$h_dibv@^^i5%g5u1*5o(7rol@$7i$^p@82o5#ibde_gk9k4m('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
