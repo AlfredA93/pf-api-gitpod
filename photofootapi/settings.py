f6f630f29332abd4ffc0cb7bd75ab2d5dac472f0
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-25 10:50:17 +0200

    Edit Allowed Hosts on settings.py

--- photofootapi/settings.py
+++ photofootapi/settings.py
@@ -64,7 +64,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), '127.0.0.1']
+ALLOWED_HOSTS = [
+    os.environ.get("ALLOWED_HOSTS"),
+    '127.0.0.1',
+    'localhost:3000'
+    ]
 
 
 # Application definition
