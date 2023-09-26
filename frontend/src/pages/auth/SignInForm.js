6e49233dddaf8180a15eef169666249a6069aeae
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-26 12:58:57 +0200

    Refactor SignInForm.js

--- frontend/src/pages/auth/SignInForm.js
+++ frontend/src/pages/auth/SignInForm.js
@@ -33,7 +33,7 @@ function SignInForm() {
       await axios.post("/dj-rest-auth/login/", signInData);
       history.push("/");
     } catch (err) {
-        setErrors(err.response?.data);
+      setErrors(err.response?.data);
     }
   };
 
@@ -62,9 +62,11 @@ function SignInForm() {
                 onChange={handleChange}
               />
             </Form.Group>
-            {errors.username?.map((message, idx) =>
-             <Alert variant="warning" key={idx}>{message}</Alert>
-            )}
+            {errors.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
@@ -77,20 +79,22 @@ function SignInForm() {
                 onChange={handleChange}
               />
             </Form.Group>
-            {errors.password?.map((message, idx) =>
-             <Alert variant="warning" key={idx}>{message}</Alert>
-            )}
+            {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
               type="submit"
             >
-              Sign In
+              Sign in
             </Button>
-            {errors.non_field_errors?.map((message, idx) =>
-             <Alert variant="warning" key={idx} className="m-2">
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="m-2">
                 {message}
-            </Alert>
-            )}
+              </Alert>
+            ))}
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
