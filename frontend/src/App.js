a446445f765132419ad151d271c6c090705df3ad
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-26 10:10:05 +0200

    Add Route 'page not found' to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
+import './api/axiosDefaults';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
           <Route exact path="/" render={() => <h1>Home Page</h1> } />
           <Route exact path="/signin" render={() => <h1>Sign In</h1> } />
           <Route exact path="/signup" render={() => <h1>Sign Up</h1> } />
+
+          <Route render={()=><p>Page not found</p>} />
         </Switch>
       </Container>
     </div>
