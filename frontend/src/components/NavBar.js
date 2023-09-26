2d64a644cd97ece9e0955b78d4f024b6760c8984
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-26 09:49:34 +0200

    Add NavLink components and classes to navbar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,19 +1,43 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import pflogo from '../assets/pflogo.PNG'
-import styles from '../styles/NavBar.module.css'
+import pflogo from '../assets/pflogo.PNG';
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand><img src={pflogo} alt="logo" height="50" /></Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={pflogo} alt="logo" height="50" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link><i className="fa-solid fa-house"></i>Home</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-arrow-right-to-bracket"></i>Sign in</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
+            <NavLink 
+            to="/" 
+            exact 
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-house"></i>Home
+            </NavLink>
+            <NavLink 
+            to="/signin" 
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-arrow-right-to-bracket"></i>Sign in
+            </NavLink>
+            <NavLink 
+            to="/signup" 
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
