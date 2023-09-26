dc0230a774a4604d1f150d98bdd1c0682a83083a
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-26 12:40:36 +0200

    Add onSubmit param to Form - SignInForm.js

--- frontend/src/pages/auth/SignInForm.js
+++ frontend/src/pages/auth/SignInForm.js
@@ -48,7 +48,8 @@ function SignInForm() {
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign in</h1>
-          <Form>
+
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
